a5d6f80b611cb4aa96f8d40fba891d76ca4560e6
Octa Toriq <octa.toriq@gmail.com>
2025-02-26 23:39:15 +0700

    add movie search functionality with API integration and new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Search from "./components/Search";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  }}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [ErrorMessage, setErrorMessage] = useState(null);
+  const [movieList, setmovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage(null);
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Something went wrong while fetching the data");
+      }
+      const data = await response.json();
+      if (data.Response === "False") {
+        setErrorMessage(
+          data.Error || "Something went wrong while fetching the data"
+        );
+        setmovieList([]);
+        return;
+      }
+      setmovieList(data.results || []);
+
+    } catch (error) {
+      console.error(`Error Fetching Movies: ${error}`);
+      setErrorMessage(`error Fetching Movies: ${error}`);
+    }
+
+    finally {
+      setIsLoading(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
-      <div className='pattern' />
-      <div className='wrapper' />
-       <header>
-          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy</h1>
-       </header>
-    </main>
-  )
-}
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : ErrorMessage ? (
+            <p className="text-red-500">{ErrorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) =>(
+                <p key={movie.id} className="text-white">{movie.title}</p>
 
-export default App
+              ))}
+            </ul>
+          )
+          }
+
+        </section>
+      </div>
+    </main>
+  );
+
+
+  };
+
+ 
+
+export default App;
