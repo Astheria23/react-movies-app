942514853a5d689bf4248aa806c2c901e0d83dde
Octa Toriq <octa.toriq@gmail.com>
2025-02-27 17:05:13 +0700

    refactor: clean up code formatting and enhance updateSearchCount function for better search term management
    fix: search doesn't showed up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
-import { useDebounce }   from "react-use";
+import { useDebounce } from "react-use";
 import { updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
@@ -17,15 +17,21 @@ const API_OPTIONS = {
 };
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [ErrorMessage, setErrorMessage] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [ErrorMessage, setErrorMessage] = useState("");
   const [movieList, setmovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
-  useDebounce(() => {setDebouncedSearchTerm(searchTerm),500,[searchTerm]})
+  useDebounce(
+    () => {
+      setDebouncedSearchTerm(searchTerm);
+    },
+    500,
+    [searchTerm]
+  );
 
-  const fetchMovies = async (query = '') => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage(null);
 
@@ -46,8 +52,10 @@ const App = () => {
         return;
       }
       setmovieList(data.results || []);
-      updateSearchCount();
 
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error Fetching Movies: ${error}`);
       setErrorMessage(`error Fetching Movies: ${error}`);
